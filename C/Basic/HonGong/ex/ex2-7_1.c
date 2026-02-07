7af28e54dbb187cc55cd4708c745587480047877
Kudeurae <titan1152@naver.com>
2026-02-07 21:15:51 +0900

    Create ex2-7_1.c
    ex2-7_1

---
+++ C/Basic/HonGong/ex/ex2-7_1.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h> 
+int main()
+{
+	printf("%c\n", 'A'); // 문자 
+	printf("%s\n", "A"); // 문자열 A\0 <-- 두 문자, \0 - NULL문자 
+
+	printf("%c은 %s입니다.\n", '1', "first"); 
+	printf("%c %d %c %c\n", '0', '0', '1', '1');
+
+	//유니 코드(utf-8), 아스키코드(ASCII) 
+
+	printf("%c%c%c%c%c\n", 'H', 'e', 'l', 'l', 'o'); 
+	printf("%s\n", "Hello"); // "Hello\0" 
+  
+	return 0;
+}
