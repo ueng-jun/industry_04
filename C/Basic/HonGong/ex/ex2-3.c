29659fdd1737b0417ae899dd30ba35840e3ab48d
Kudeurae <titan1152@naver.com>
2026-02-07 14:09:16 +0900

    Create ex2-3.c
    p057 ex2-3

---
+++ C/Basic/HonGong/ex/ex2-3.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h> 
+int main()
+{
+
+	printf("Be happy\n"); //"Be happy"룰 출력하고 줄을 바꿈(\n) 
+	printf("12345678901234567890\n"); //"화면에 열 번호를 입력하고 줄을 바꿈(\n) 
+	printf("My\tfriend\n"); 
+	//"My"를 출력하고 탭 위치로 이동(t\) 후에 "friend"를 출력하고 줄을 바꿈(\n) 
+	printf("Goot\bd\tchance\n"); 
+	//t를 d로 바꾸고 탭 위치로 이동(t\) 후에 "chance"를 출력하고 줄을 바꿈(\n) 
+	printf("Cow\rW\a\n"); 
+	//맨 앞으로 이동(\r)해 C를 W로 바꾸고 벨소리(\a)를 내고 줄을 바꿈(\n) 
+
+	return 0;
+}
